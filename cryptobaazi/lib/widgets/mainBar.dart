f54f3875de1f82c5ee82211ea0a09c4bf9ebbf9f
Shikhar Jain <38657722+Shikhar0018@users.noreply.github.com>
2021-05-22 20:06:52 +0530

    minor

--- cryptobaazi/lib/widgets/mainBar.dart
+++ cryptobaazi/lib/widgets/mainBar.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:velocity_x/velocity_x.dart';
-
 import 'homepage_widgets/home.dart';
 import 'homepage_widgets/discover.dart';
 import 'homepage_widgets/list_all.dart';
